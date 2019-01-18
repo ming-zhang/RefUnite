d5012ad2072c163895a7d06009bad1a273575af5
Abha Vedula <abhavedula@nichols-ve506-0040.apn.wlan.upenn.edu>
2019-01-18 14:52:34 -0500

    Scrape all imgs in page

--- scrapeTraceTheFace.py
+++ scrapeTraceTheFace.py
@@ -11,15 +11,11 @@ page = urlopen(page_url)
 # parse the html using beautiful soup 
 soup = BeautifulSoup(page, 'html.parser')
 
-# Get div containing image
-img_div = soup.find('div', attrs={'class': 'picture-result mobileFullWidth'})
+img_divs = soup.find_all('div', attrs={'class': 'picture-result mobileFullWidth'})
 
-# Get img src
-img_url = img_div.find('img')['src']
-
-# Get id
-img_id = img_div.find('strong').text.strip()
-
-# Print image url and id
-print('https://familylinks.icrc.org/' + img_url)
-print(img_id)
+for img_div in img_divs:
+	img_url = img_div.find('img')['src']
+	img_id = img_div.find('strong').text.strip()
+	print('url: https://familylinks.icrc.org/' + img_url)
+	print('id: ' + img_id)
+	# TODO: store in AWS

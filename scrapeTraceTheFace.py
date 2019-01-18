5d39186f27277a896d283879222d611344e5d891
Abha Vedula <abhavedula@nichols-ve506-0040.apn.wlan.upenn.edu>
2019-01-18 15:01:14 -0500

    download all images with id instead of scraping

--- scrapeTraceTheFace.py
+++ scrapeTraceTheFace.py
@@ -1,21 +1,24 @@
-# import libraries
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+# # import libraries
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
 
-# specify the url
-page_url = 'https://familylinks.icrc.org/europe/en/Pages/search-persons.aspx'
+# # specify the url
+# page_url = 'https://familylinks.icrc.org/europe/en/Pages/search-persons.aspx'
 
-# open page
-page = urlopen(page_url)
+# # open page
+# page = urlopen(page_url)
 
-# parse the html using beautiful soup 
-soup = BeautifulSoup(page, 'html.parser')
+# # parse the html using beautiful soup 
+# soup = BeautifulSoup(page, 'html.parser')
 
-img_divs = soup.find_all('div', attrs={'class': 'picture-result mobileFullWidth'})
+# img_divs = soup.find_all('div', attrs={'class': 'picture-result mobileFullWidth'})
 
-for img_div in img_divs:
-	img_url = img_div.find('img')['src']
-	img_id = img_div.find('strong').text.strip()
-	print('url: https://familylinks.icrc.org/' + img_url)
-	print('id: ' + img_id)
-	# TODO: store in AWS
+# for img_div in img_divs:
+# 	img_url = img_div.find('img')['src']
+# 	img_id = img_div.find('strong').text.strip()
+# 	print('url: https://familylinks.icrc.org/' + img_url)
+# 	print('id: ' + img_id)
+# 	# TODO: store in AWS
+
+import urllib.request 
+data = urllib.request.urlretrieve("https://familylinks.icrc.org//europe/PersonImages/0007181.watermarked.jpg", "0007181.jpg")

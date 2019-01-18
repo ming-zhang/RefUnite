664cca4f3683b1fc7a4492e1f2063f071c1a5aed
Ming Zhang <mingzha@seas.upenn.edu>
2019-01-18 15:37:37 -0500

    Add s3 functions

--- scrapeTraceTheFace.py
+++ scrapeTraceTheFace.py
@@ -1,21 +1,17 @@
-# import libraries
-# from urllib.request import urlopen
-# from bs4 import BeautifulSoup
 import urllib.request, urllib.error
+import boto3
 
 # trace the face
 ttf_api = 'https://familylinks.icrc.org//europe/PersonImages/{i:07}.jpg'
 
-# s3 = boto3.resource('s3')
-# s3.meta.client.download_file('esuds-bucket', 'data.csv', '/tmp/data.csv')
+s3 = boto3.resource('s3')
 
-for i in range(7000, 7100):
+for i in range(7000, 7010):
     url = ttf_api.format(i=i)
     filename = url[-11:]
     try:
         urllib.request.urlretrieve(url, filename)
         print('downloaded ' + filename)
+        s3.meta.client.upload_file('./%s' % filename, 's3-hello-world-mingzhang', filename)
     except urllib.error.URLError:
         print('can\'t find ' + filename)
-
-# s3.meta.client.upload_file('/tmp/data.csv', 'esuds-bucket', 'data.csv')

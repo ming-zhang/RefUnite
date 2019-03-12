3e7dd1ebc93e112c62f2e4b9cd5b342d5c489e03
Kellie <carlsonk@seas.upenn.edu>
2019-03-12 19:57:14 -0400

    basic web app structure

---
+++ NodeApp/public/javascripts/app.js
@@ -0,0 +1,46 @@
+var app = angular.module('angularjsNodejsTutorial', []);
+app.controller('loginController', function($scope, $http) {
+  $scope.verifyLogin = function() {
+    // To check in the console if the variables are correctly storing the input:
+    // console.log($scope.username, $scope.password);
+
+    var request = $http({
+      url: '/login',
+      method: "POST",
+      data: {
+        'username': $scope.username,
+        'password': $scope.password
+      }
+    })
+
+    request.success(function(response) {
+      // success
+      console.log(response);
+      if (response.result === "success") {
+        // After you've written the INSERT query in routes/index.js, uncomment the following line
+        window.location.href = "http://localhost:8081/dashboard"
+      }
+    });
+    request.error(function(err) {
+      // failed
+      console.log("error: ", err);
+    });
+
+  };
+});
+
+// Template for adding a controller
+/*
+app.controller('dummyController', function($scope, $http) {
+  // normal variables
+  var dummyVar1 = 'abc';
+
+  // Angular scope variables
+  $scope.dummyVar2 = 'abc';
+
+  // Angular function
+  $scope.dummyFunction = function() {
+
+  };
+});
+*/

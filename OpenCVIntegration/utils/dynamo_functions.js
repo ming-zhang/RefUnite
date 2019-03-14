5fa9f1a3b28f1320503fa2fb4b2582543740581f
Sanjana Sarkar <sanjsark1@gmail.com>
2019-03-14 19:47:23 -0400

    changed schema famfriend + user

--- OpenCVIntegration/utils/dynamo_functions.js
+++ OpenCVIntegration/utils/dynamo_functions.js
@@ -11,7 +11,7 @@ kvs.init(function(err, message) {
 });
 
 // add a user to the database with the given params (JSON document)
-var addUser = function(email, password, gender, age, origin, looking_for, img_ids, callback) {
+var addUser = function(email, password, gender, age, origin, looking_for, img_ids, fam_friend_ids, callback) {
 	var params = {
 		email: email,
 		password: password,
@@ -19,7 +19,8 @@ var addUser = function(email, password, gender, age, origin, looking_for, img_id
 		age: age,
 		origin: origin,
 		looking_for: looking_for,
-		img_ids: img_ids
+		img_ids: img_ids,
+		fam_friend_ids: fam_friend_ids
 	}
 	kvs.postUser(params, function(err, data) {
 		if (err) {
@@ -46,13 +47,14 @@ var getUser = function(email, callback) {
 }
 
 // add a famFriend to the database with the given params (JSON document)
-var addFamFriend = function(img_ids, relation, gender, age, origin) {
+var addFamFriend = function(img_ids, relation, gender, age, origin, img_ids) {
 	var params = {
 		img_ids: img_ids,
 		relation: relation,
 		gender: gender,
 		age: age,
-		origin: origin
+		origin: origin,
+		img_ids: img_ids
 	}
 	kvs.postFamFriend(params, function(err, data) {
 		if (err) {

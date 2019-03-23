c812f619b7d0989f84b3f3aa4a566305a54fdc85
Reeham Sedky <reehams@seas.upenn.edu>
2019-03-23 17:21:30 -0400

    Update routes.js

--- OpenCVIntegration/routes/routes.js
+++ OpenCVIntegration/routes/routes.js
@@ -1,192 +1,68 @@
-
-var express = require('express');
-var router = express.Router();
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+var fs = require('fs');
+var https = require('https');
 var download = require('download-file');
-var s3 = require('../utils/s3_functions'); 
-
-const path = require('path');
-const fs = require('fs');
 const fr = require('face-recognition');
 
-// For New Data Set ,Uncomment the code below to :
-// 1.Detect Faces from complete images
-// 2.Save 150x150 faces with class names for each
-// 3.Use Some of these to train the classifier
-// 4.Save Trained Data into Model.json
-// 5.Test for remaining faces
+aws.config.update({
+  accessKeyId: "AKIAIBIOLFSQYUBEA7XQ",
+  secretAccessKey: "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",
+  region: 'us-east-1'
+});
 
+const s3 = new aws.S3();
 
-/*
-Below Works Perfectly to Extract 150x150 faces from images and saves them to faces
-*///Manually change this path
-//console.log("Meow");  
-
-var url = 'https://s3.amazonaws.com/tracetheface/0000012.jpg'; 
-
-var options = {
-  directory: './pictures/',
-  filename: 'tester.jpg'
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    console.log('file filter got this file')
+    console.log(file)
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+  }
 }
 
-console.log("Hello"); 
-//Folder for training data when we have upload functionality working
-download(url, options, function(err){
-  console.log("We are inside"); 
-  const image = fr.loadImage('./pictures/tester.jpg');
-  const detector = fr.FaceDetector();
-  const targetSize = 150;
-  const faceImages = detector.detectFaces(image, targetSize);
-  //Save faceImages to pictures/faces/face_0.jpg ++
-  const uniqueId = url.substring(38,45); 
-  faceImages.forEach((img, i) => fr.saveImage(`./pictures/faces/${uniqueId}_${i}.png`,img));
-  fs.unlinkSync('./pictures/tester.jpg'); 
-  if(err) throw err  
-})
-console.log("WE are after"); 
-
-//Folder for testing data- Trace The Face Database 
-s3.getImages(); 
-/*
-Now Manually Rename the faces from [face_1].png to [CorrectName]_1.png
-*/
-
-/*
-Optional
-Open Xquartz for below 3 lines to display mesh of faces
-*/
-// const win = new fr.ImageWindow();
-// win.setImage(fr.tileImages(faceImages));
-// fr.hitEnterToContinue();
-
-
-/*
-We have 4-5 pictures for each character
-we use 4 to train and rest to test our training
-*/
-const dataPath = path.resolve('./pictures/faces');
-//Our 'database' (add the back-end here)
-//Dummy database 
-//ClassNames has to be changed to our uniqueIdentifiers for famFriends 
-const classNames = ['Linkin', 'Michael', 'Sucre', 'Tbag','nishant','reeham'];
-const allFiles = fs.readdirSync(dataPath);
-const imagesByClass = classNames.map(c =>
-   allFiles
-     .filter(f => f.includes(c))
-     .map(f => path.join(dataPath, f))
-     .map(fp => fr.loadImage(fp))
- );
-//Maybe change later to 4 
- const numTrainingFaces = 2;
- const trainDataByClass = imagesByClass.map(imgs => imgs.slice(0, numTrainingFaces));
- const testDataByClass = imagesByClass.map(imgs => imgs.slice(numTrainingFaces));
-
-/*
-Train our recognizer
-*/
- const recognizer = fr.FaceRecognizer();
-
- trainDataByClass.forEach((faces, label) => {
-   const name = classNames[label];
-   recognizer.addFaces(faces, name);
- });
-
-/*
-Save Our Trained Data
-*/
- const modelState = recognizer.serialize();
-fs.writeFileSync('model.json', JSON.stringify(modelState));
-
-/*
-Load Our Previously Saved Train Data
-*/
-// const modelState = require('../model.json');
-// recognizer.load(modelState);
-
-
-/*
-Recognizing New Faces
-*/
-// const errors = classNames.map(_ => []);
-// testDataByClass.forEach((faces, label) => {
-//   const name = classNames[label];
-//   console.log();
-//   console.log('testing %s', name);
-//   faces.forEach((face, i) => {
-//     const prediction = recognizer.predictBest(face);
-//     console.log('%s (%s)', prediction.className, prediction.distance);
-//
-//     // count number of wrong classifications
-//     if (prediction.className !== name) {
-//       errors[label] = errors[label] + 1;
-//     }
-//   });
-// });
-
-/*
-print the result
-*/
-// const result = classNames.map((className, label) => {
-//   const numTestFaces = testDataByClass[label].length;
-//   const numCorrect = numTestFaces - errors[label].length;
-//   const accuracy = parseInt((numCorrect / numTestFaces) * 10000) / 100;
-//   return `${className} ( ${accuracy}% ) : ${numCorrect} of ${numTestFaces} faces have been recognized correctly`;
-// })
-// console.log('result:');
-// console.log(result);
-
-
-router.get('/',function(req, res){
-  res.render('index');
+const upload = multer({
+  fileFilter,
+  storage: multerS3({
+    acl: 'bucket-owner-full-control',
+    s3,
+    bucket: 'refunite-user-images',
+    /* metadata: function (req, file, cb) {
+      cb(null, {});
+    }, */
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
 });
 
-router.get('/recognize',function(req,res){
-
-  const recognizer = fr.FaceRecognizer();
-  /*
-  Load Our Previously Saved Train Data
-  */
-  const modelState = require('../model.json');
-  recognizer.load(modelState);
-
-  /*
-  Detect Face From Image
-  */
-  const image = fr.loadImage('./tmp/uploads/out.png');
-  const detector = fr.FaceDetector();
-  const targetSize = 150;
-  const faceImage = detector.detectFaces(image, targetSize);
-
-  /*
-  draw rect on Face
-  and write prediction on each Face
-  */
-  const faceRects  = detector.locateFaces(image).map(mmodRect => mmodRect.rect);
-  const faces = detector.getFacesFromLocations(image, faceRects, 150);
-
-  if(faceRects.length){
-    const win= new fr.ImageWindow();
-    win.setImage(image);
-    faceRects.forEach((rect,i)=>{
-      win.addOverlay(rect);
-      const predict = recognizer.predictBest(faces[i],0.69);
-       win.addOverlay(rect, `${predict.className} (${predict.distance})`);
-
+const testImages = () => {
+  var params = {Bucket: 'tracethefacetest'};
+  s3.listObjects(params, function(err, data){
+    var bucketContents = data.Contents;
+      for (var i = 0; i < bucketContents.length; i++){
+        var urlParams = {Bucket: 'tracetheface', Key: bucketContents[i].Key};
+          s3.getSignedUrl('getObject',urlParams, function(err, url){
+            console.log('the url of the image is', url);
+            // Save image to folder
+            const uniqueId = url.substring(38,49); 
+            var options = {
+              directory: './pictures/testing',
+              filename: uniqueId 
+            }
+            console.log("Whats up"); 
+            //Folder for training data when we have upload functionality working
+            download(url, options, function(err){
+              if(err) throw err  
+            })
+          });
+      }
   });
-    // fr.hitEnterToContinue();
-  }
+}
 
-  /*
-  Send Output for one face to html page
-  */
-  if(faceImage.length){
-  const predictions=recognizer.predict(faceImage[0]);
-  res.send(predictions);
-  }
-  else{
-    res.status(400).json({msg:'Could Not Detect Face, Please try another picture'});
-  }
-
-});
-
-
-module.exports = router;
+module.exports = {upload: upload,
+                  getImages: testImages
+                 };
